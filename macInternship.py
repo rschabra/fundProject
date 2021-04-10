f787ab34abed20ae9cf4bbe029a7c33d210f9c8e
Rohan Chabra <rschabra@umd.edu>
2021-04-10 12:39:50 -0400

    Changes to searching all respositories and including the stars and forks of the repositories in a print function.

--- macInternship.py
+++ macInternship.py
@@ -2,5 +2,5 @@ from github import Github
 
 g = Github('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')
 
-for repo in g.get_user().get_repos():
-    print(repo.name)
+for repo in g.search_repositories('stars:3500..4000 forks:>100', 'stars', 'desc'):
+    print(repo.name + ', Stars: ' + str(repo.stargazers_count) + ', Forks: ' + str(repo.forks))
