00f3cd6d606d23364c5f56dfa8bc3571ab42a296
Rohan Chabra <rschabra@umd.edu>
2021-04-11 02:56:20 -0400

    Added most recent commit, WIP for timedelta to days

--- ghub_pull.py
+++ ghub_pull.py
@@ -2,7 +2,7 @@ import time
 from github import Github
 from csv import writer
 from array import *
-from datetime import date
+import datetime
 t0 =  time.time()
 
 g = Github('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')
@@ -18,12 +18,11 @@ def append_arr_as_row(file_name, arr_elem, newline=''):
         csv_writer.writerow(arr_elem)
 
 def get_commit_date(repo):
-    commits = repo.get_commits('sort:author-data-desc')
-    if(len(commits) > 0):
-        last_commit = commits[0].commit.author.date
-        repo_arr[5] = commits.totalCount
-        today = date(2021, 4, 11)
-    return today - last_commit
+    commits = repo.get_commits()
+    data = commits.get_page(0)
+    last_commit = data[0].commit.author.date.date()
+    today = datetime.datetime.now().date()
+    return (today - last_commit), commits.totalCount
 
 for repo in g.search_repositories('stars:4450..4500 forks:>10 created:>2018-01-01', 'stars', 'desc'):
     repo_arr[0] = repo.name
@@ -31,7 +30,9 @@ for repo in g.search_repositories('stars:4450..4500 forks:>10 created:>2018-01-0
     repo_arr[2] = repo.forks
     repo_arr[3] = repo.get_pulls(state='closed', sort='created', direction='asc').totalCount
     repo_arr[4] = repo.get_pulls(state='open', sort='created', direction='asc').totalCount
-    repo_arr[5] = get_commit_date(repo)
+    repo_arr[5] = get_commit_date(repo)[1]
+    repo_arr[6] = get_commit_date(repo)[0]
+
 
 
     append_arr_as_row('github_list.csv', repo_arr)
