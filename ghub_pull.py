f34d743464c9c344997ee5209e2b9c4b0a57ed34
Rohan Chabra <rschabra@umd.edu>
2021-04-17 16:44:11 -0400

    Added #Contributors, #WorkflowRuns, Topics

--- ghub_pull.py
+++ ghub_pull.py
@@ -6,11 +6,12 @@ import datetime
 t0 =  time.time()
 
 g = Github('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')
-repo_arr = [0]*7
+repo_arr = [0]*10
 
 with open('github_list.csv', 'w', newline='') as write_arr:
         csv_writer = writer(write_arr)
-        csv_writer.writerow(['Name', 'Stars', 'Forks', 'Closed Pulls Count', 'Open Pulls Count', 'Number of Commits', 'Days Since Last Commit'])
+        csv_writer.writerow(['Name', 'Stars', 'Forks', 'Closed Pulls Count', 'Open Pulls Count', 
+        'Number of Commits', 'Days Since Last Commit', 'Number of Contributors', 'Number of Workflow Runs', 'Topics'])
 
 def append_arr_as_row(file_name, arr_elem, newline=''):
     with open(file_name, 'a+', newline='') as write_arr:
@@ -27,7 +28,7 @@ def get_commit_date(repo):
     days = commit_date.split(' ')
     return days[0], commits.totalCount
 
-for repo in g.search_repositories('stars:4200..4500 forks:>10 created:>2018-01-01', 'stars', 'desc'):
+for repo in g.search_repositories('stars:4000..4500 forks:>10 created:>2020-01-01', 'stars', 'desc'):
     repo_arr[0] = repo.name
     repo_arr[1] = repo.stargazers_count
     repo_arr[2] = repo.forks
@@ -35,6 +36,9 @@ for repo in g.search_repositories('stars:4200..4500 forks:>10 created:>2018-01-0
     repo_arr[4] = repo.get_pulls(state='open', sort='created', direction='asc').totalCount
     repo_arr[5] = get_commit_date(repo)[1]
     repo_arr[6] = get_commit_date(repo)[0]
+    repo_arr[7] = repo.get_contributors().totalCount
+    repo_arr[8] = repo.get_workflow_runs().totalCount
+    repo_arr[9] = repo.get_topics()
 
 
     append_arr_as_row('github_list.csv', repo_arr)
